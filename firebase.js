9f76431ff7250be4e47e55eb81e61b027168aa07
Zhihan1996 <zzh760998379@163.com>
2021-01-18 14:12:41 -0600

    fetch data from database

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+
+import "firebase/database";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBVibuqj0j7B5utKiTKC4yl2gd3hZZyki0",
+    authDomain: "scheduler-7bd75.firebaseapp.com",
+    databaseURL: "https://scheduler-7bd75-default-rtdb.firebaseio.com",
+    projectId: "scheduler-7bd75",
+    storageBucket: "scheduler-7bd75.appspot.com",
+    messagingSenderId: "1035883013034",
+    appId: "1:1035883013034:web:9f6ab4c64a8270ef99c1d3",
+    measurementId: "G-NQZXSV20PK"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export { firebase };
--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import CourseList from '../components/CourseList';
 import UserContext from '../UserContext';
 import CourseEditScreen from './CourseEditScreen';
-
+import {firebase} from '../firebase'
 
 
 
@@ -14,10 +14,24 @@ const Banner = ({title}) => (
 
 const ScheduleScreen = ({navigation}) => {
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+//   const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
 
   const user = useContext(UserContext);
   const canEdit = user && user.role === 'admin';
+
+  const fixCourses = json => ({
+    ...json,
+    courses: Object.values(json.courses)
+  });
+
+  useEffect(() => {
+    const db = firebase.database().ref();
+    const handleData = snap => {
+      if (snap.val()) setSchedule(fixCourses(snap.val()));
+    }
+    db.on('value', handleData, error => alert(error));
+    return () => { db.off('value', handleData); };
+  }, []);
 
 
 
@@ -25,15 +39,15 @@ const ScheduleScreen = ({navigation}) => {
     navigation.navigate(canEdit ? 'CourseEditScreen' : 'CourseDetailScreen', {course});
   };
 
-  useEffect(() => {
-    const fetchSchedule = async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(json);
-    }
-    fetchSchedule();
-  }, [])
+//   useEffect(() => {
+//     const fetchSchedule = async () => {
+//       const response = await fetch(url);
+//       if (!response.ok) throw response;
+//       const json = await response.json();
+//       setSchedule(json);
+//     }
+//     fetchSchedule();
+//   }, [])
 
   return (
     <SafeAreaView style={styles.container}>
